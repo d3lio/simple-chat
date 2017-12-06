c824bf92842be6a86925f177696eda9d70930edc
Delyan Dobrev <delqn.dobrev@gmail.com>
2017-12-06 14:39:45 +0200

    Working simple chat system

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,60 @@
+use std::io::{self, ErrorKind, Read, Write};
 use std::net::TcpStream;
-use std::io::Write;
+use std::sync::mpsc::{self, TryRecvError};
+use std::thread;
+use std::time::Duration;
+
+const LOCALHOST: &str = "127.0.0.1:1234";
+const MESSAGE_SIZE: usize = 32;
 
 fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:12345").unwrap();
+    let mut client = TcpStream::connect(LOCALHOST).expect("Stream failed to connect");
+    client.set_nonblocking(true).expect("Failed to initialize nonblocking");
 
-    for i in 0..10 {
-        let size = 10u64;
-        stream.write_all(unsafe { &::std::mem::transmute::<u64, [u8; 8]>(u64::to_le(size)) }).unwrap();
-        stream.write_fmt(format_args!("0123456789")).unwrap();
-        println!("sent message no. {}", i);
+    let (sx, rx) = mpsc::channel::<String>();
+
+    thread::spawn(move || loop {
+        let mut buf = vec![0; MESSAGE_SIZE];
+
+        // Try to receive message from server
+        match client.read_exact(&mut buf) {
+            Ok(_) => {
+                let msg = buf.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+                println!("message recv {:?}", msg);
+            },
+            Err(ref err) => match err.kind() {
+                ErrorKind::WouldBlock => (),
+                ErrorKind::UnexpectedEof => break,
+                _ => {
+                    println!("Connection with the server closed");
+                    break;
+                }
+            }
+        }
+
+        // Try to send message from repl
+        match rx.try_recv() {
+            Ok(msg) => {
+                let mut buf = msg.clone().into_bytes();
+                buf.resize(MESSAGE_SIZE, 0);
+                client.write_all(&buf).expect("Writing to socket failed");
+                println!("message sent {:?}", msg);
+            },
+            Err(TryRecvError::Empty) => (),
+            Err(TryRecvError::Disconnected) => break
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    });
+
+    println!("repl");
+    loop {
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).expect("Reading form stdin failed");
+        let msg = buf.trim().to_string();
+
+        if msg == ":q" || sx.send(msg).is_err() { break }
     }
+    println!("bye!");
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,234 +1,64 @@
-use std::io::{Read, Write, ErrorKind};
-use std::net::{TcpListener, TcpStream};
-use std::sync::mpsc::{channel, Sender, Receiver, TryRecvError};
+use std::io::{ErrorKind, Read, Write};
+use std::net::TcpListener;
+use std::sync::mpsc;
 use std::thread;
 
-const URL: &str = "127.0.0.1:12345";
-const BUFFER_SIZE: usize = 128;
-
-#[derive(Debug, PartialEq, Eq)]
-enum MessagePhase {
-    Header,
-    Payload
+fn sleep() {
+    thread::sleep(::std::time::Duration::from_millis(100));
 }
 
-#[derive(Debug)]
-struct Message {
-    size: u64,
-    buffer: Vec<u8>,
-    phase: MessagePhase
-}
-
-impl Message {
-    pub fn new() -> Self {
-        Self {
-            size: 0,
-            buffer: Vec::with_capacity(BUFFER_SIZE),
-            phase: MessagePhase::Header
-        }
-    }
-
-    // TODO bufreader
-    fn read_header(&mut self, stream: &mut TcpStream) -> bool {
-        if self.phase != MessagePhase::Header {
-            return false;
-        }
-
-        self.size = unsafe {
-            let mut buffer = [0u8; 8];
-            if let Err(err) = stream.read_exact(&mut buffer) {
-                match err.kind() {
-                    ErrorKind::WouldBlock => return false,
-                    ErrorKind::UnexpectedEof => {
-                        self.phase = MessagePhase::Payload;
-                        return true;
-                    },
-                    _ => panic!("{:?}", err)
-                }
-            }
-
-            let num = ::std::mem::transmute(buffer);
-            if num == 0 {
-                return true;
-            }
-            u64::from_le(num)
-        };
-
-        self.phase = MessagePhase::Payload;
-        true
-    }
-
-    // TODO bufreader
-    fn read_payload(&mut self, stream: &mut TcpStream) -> bool {
-        if self.phase != MessagePhase::Payload {
-            return false;
-        }
-
-        if self.size > self.buffer.len() as u64 {
-            if self.buffer.capacity() - self.buffer.len() < BUFFER_SIZE {
-                let cap = self.buffer.capacity();
-                self.buffer.resize(cap + BUFFER_SIZE, 0);
-            }
-
-            let orig_len = self.buffer.len();
-            let cap = self.buffer.capacity();
-            unsafe {
-                self.buffer.set_len(cap);
-                match stream.read(&mut self.buffer[orig_len..(orig_len + BUFFER_SIZE)]) {
-                    Ok(len) => {
-                        self.buffer.set_len(orig_len + len);
-                        if self.size <= self.buffer.len() as u64 {
-                            true
-                        } else {
-                            false
-                        }
-                    },
-                    Err(err) => {
-                        self.buffer.set_len(orig_len);
-                        if err.kind() == ErrorKind::WouldBlock {
-                            false
-                        } else {
-                            panic!(err);
-                        }
-                    }
-                }
-            }
-        } else {
-            true
-        }
-    }
-
-    pub fn read(&mut self, stream: &mut TcpStream) -> bool {
-        match self.phase {
-            MessagePhase::Header => self.read_header(stream) && self.read_payload(stream),
-            MessagePhase::Payload => self.read_payload(stream)
-        }
-    }
-}
-
-/*fn read_proto(stream: &mut TcpStream) -> (u64, Vec<u8>) {
-    let message_size: u64 = unsafe {
-        let mut buffer = [0u8; 8];
-        stream.read_exact(&mut buffer).unwrap();
-
-        let num = ::std::mem::transmute(buffer);
-        if num == 0 {
-            return (0, Vec::new());
-        }
-        u64::from_le(num)
-    };
-
-    let mut buffer = Vec::with_capacity(BUFFER_SIZE);
-    while (buffer.len() as u64) < message_size {
-        if buffer.capacity() - buffer.len() < BUFFER_SIZE {
-            let cap = buffer.capacity();
-            buffer.resize(cap + BUFFER_SIZE, 0);
-        }
-
-        let orig_len = buffer.len();
-        let cap = buffer.capacity();
-        unsafe {
-            buffer.set_len(cap);
-            let len = stream.read(&mut buffer[orig_len..(orig_len + BUFFER_SIZE)]).unwrap();
-            buffer.set_len(orig_len + len);
-        }
-    }
-    (message_size, buffer)
-}*/
-
-struct BiChannel<T> {
-    pub sender: Sender<T>,
-    pub receiver: Receiver<T>
-}
-
-impl<T> BiChannel<T> {
-    pub fn new() -> (Self, Self) {
-        let (sender1, receiver1) = channel();
-        let (sender2, receiver2) = channel();
-        (Self {
-            sender: sender1,
-            receiver: receiver2
-        },
-        Self {
-            sender: sender2,
-            receiver: receiver1
-        })
-    }
-}
+const LOCALHOST: &str = "127.0.0.1:1234";
+const MESSAGE_SIZE: usize = 32;
 
 fn main() {
-    let server = TcpListener::bind(URL).unwrap();
-    server.set_nonblocking(true).unwrap();
+    let server = TcpListener::bind(LOCALHOST).expect("Listener failed to bind");
+    server.set_nonblocking(true).expect("Failed to initialize nonblocking");
+
     let mut clients = Vec::new();
+    let (sx, rx) = mpsc::channel::<String>();
 
     loop {
-        // Try to accept a connection
-        if let Ok((mut stream, addr)) = server.accept() {
-            println!("Connection from addr {}", addr);
+        // Try to accept a client
+        if let Ok((mut socket, addr)) = server.accept() {
+            println!("Client {} connected", addr);
 
-            let channel = {
-                let (a, b) = BiChannel::new();
-                clients.push(a);
-                b
-            };
+            let sx = sx.clone();
 
-            thread::spawn(move || {
-                stream.set_nonblocking(true).expect("set_nonblocking failed");
+            clients.push(socket.try_clone().expect("Failed to clone client"));
 
-                let mut message = Message::new();
+            thread::spawn(move || loop {
+                let mut buf = vec![0; MESSAGE_SIZE];
 
-                // TODO: park
-                // event loop
-                loop {
-                    if message.read(&mut stream) {
-                        if message.size == 0 {
-                            break;
-                        }
+                // Try to receive message from client
+                match socket.read_exact(&mut buf) {
+                    Ok(_) => {
+                        let msg = buf.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                        let msg = String::from_utf8(msg).expect("Invalid utf8 message");
 
-                        println!("{:?}", message);
-
-                        if message.size < message.buffer.len() as u64 {
-                            stream.write_fmt(format_args!{
-                                "Message size missmatch. Expected {} bytes, read {} bytes",
-                                message.size,
-                                message.buffer.len()
-                            }).expect("write_fmt failed");
-                        } else {
-                            channel.sender.send(message.buffer).expect("send failed");
-                        }
-
-                        message = Message::new();
+                        println!("{}: {:?}", addr, msg);
+                        sx.send(msg).expect("Send to master channel failed");
+                    },
+                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                    Err(_) => {
+                        println!("Closing connection with: {}", addr);
+                        break;
                     }
-
-                    if let Ok(data) = channel.receiver.try_recv() {
-                        stream.write_all(&data[..]).expect("write_all failed");
-                    }
-
-                    ::std::thread::sleep(::std::time::Duration::from_millis(100));
                 }
 
-                println!("Connection closed addr {}", addr);
+                sleep();
             });
         }
 
-        // let mut data = Vec::<u8>::new();
-        clients.retain(|client| {
-            match client.receiver.try_recv() {
-                Ok(data) => {
-                    println!("{:?}", data);
-                    // data = data;
-                    true
-                },
-                Err(TryRecvError::Empty) => true,
-                Err(TryRecvError::Disconnected) => false
-            }
-        });
-        // if data.len() {
-        //     for client in clients.iter() {
-        //         client.sender.send
-        //     }
-        // }
+        if let Ok(msg) = rx.try_recv() {
+            // Try to send message from master channel
+            clients = clients.into_iter().filter_map(|mut client| {
+                let mut buf = msg.clone().into_bytes();
+                buf.resize(MESSAGE_SIZE, 0);
 
-        ::std::thread::sleep(::std::time::Duration::from_millis(100));
+                client.write_all(&buf).map(|_| client).ok()
+            }).collect::<Vec<_>>();
+        }
+
+        sleep();
     }
 }
